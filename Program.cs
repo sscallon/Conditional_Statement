8f9370ef45806c5bdf8023856d152f853cd88e2f
Shane Scallon <sscallon@usf.edu>
2020-01-29 19:40:24 -0500

    Commit changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Programmer: Shane Scallon
+//Date: 1/29/2020
+//Description: C# console that demonstrates the use of conditional statements
+//after getting input from users
+using System;
 
 namespace Conditional_Statement
 {
@@ -7,7 +11,7 @@ namespace Conditional_Statement
         static void Main(string[] args)
         {
             //ask the user for input
-            Console.WriteLine("Please enter grade number");
+            Console.WriteLine("Please enter expected grade number");
 
             //use try-catch to validate the user input
             //If the user enters wrong input the catch will handle and show error message
@@ -15,22 +19,73 @@ namespace Conditional_Statement
             {
                 //input variable gathers input from user
                 string input = Console.ReadLine();
-                //
-                int Grade = int.Parse(input);
+                //Variable stores grade after input value is parsed to an interger
+                int grade = int.Parse(input);
+
+                //if else-if statement that determines the students letter grade based on 
+                //the interger grade they entered
+                if (grade > 97)
+                {
+                    Console.WriteLine("A+");
+                }
+                else if (grade > 91 && grade < 98)
+                {
+                    Console.WriteLine("A");
+                }
+                else if (grade > 89 && grade < 92)
+                {
+                    Console.WriteLine("A-");
+                }
+                else if (grade > 87 && grade < 90)
+                {
+                    Console.WriteLine("B+");
+                }
+                else if (grade > 81 && grade < 88)
+                {
+                    Console.Write("B");
+                }
+                else if (grade > 79 && grade < 82)
+                {
+                    Console.WriteLine("B-");
+                }
+                else if (grade > 77 && grade < 80)
+                {
+                    Console.WriteLine("C+");
+                }
+                else if (grade > 71 && grade <78)
+                {
+                    Console.WriteLine("C");
+                }
+                else if (grade > 69 && grade < 72)
+                {
+                    Console.WriteLine("C-");
+                }
+                else if (grade > 67 && grade < 70)
+                {
+                    Console.WriteLine("D+");
+                }
+                else if (grade > 61 && grade <68)
+                {
+                    Console.WriteLine("D");
+                }
+                else if (grade > 59 && grade < 62)
+                {
+                    Console.WriteLine("D-");
+                }
+                else
+                {
+                    Console.WriteLine("F");
+                }
 
 
-
-
-
-
-            }
+            }//end of try
             catch
             {
                 Console.WriteLine("Please enter an interger value");
                 Console.WriteLine("Press any key to exit the program...");
                 Console.ReadKey(true);
                 
-            }
+            }//end of catch
         }
     }
 }
